8853fc951ffd3e87cb6a89b8808a99bc01845328
OuachaAli23 <144799032+OuachaAli23@users.noreply.github.com>
2024-04-20 18:42:46 +0100

    Update main.cpp
    add new Point Class

--- Exercice_01/main.cpp
+++ Exercice_01/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+using namespace std ;
 
-using namespace std;
-
-int main()
+class Point // Déclaration de la classe Point
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    float x, y ;
+
+    public :
+        void initialise(float, float) ;
+        void deplace (float, float) ;
+        void affiche () ;
+} ;
+
+//Définition des fonctions membres
+void Point::initialise (float abs, float ord){
+    x = abs ; y = ord ;
 }
+void Point::deplace (float dx, float dy){
+    x = x + dx ; y = + dy ;
+}
+void Point::affiche (){
+    cout << "Mes coordonnées cartésiennes sont : " << x << " et " << y << endl ;
+}
+
+int main(){
+    Point p1;
+    p1.initialise(2.3 , 4.5);
+    p1.affiche();
+    p1.deplace(10.10, 20.20);
+    p1.affiche();
+}
+
